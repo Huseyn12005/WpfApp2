0e60ba74026ff0f2272561deee8a47c39043e3f6
Huseyn <horucov148@gmail.com>
2024-03-03 23:22:12 +0400

    Add project files.

---
+++ WpfApp2/Commands/RelayCommand.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp2.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?>? _canExecute;
+        private Func<Task<string>> getAllDataFromJsonApi;
+        private Action check;
+        private Func<object?, Task<bool>> check1;
+        private Action getAllDataFromJsonApi1;
+        private Func<object?, Task<bool>> check2;
+        private Action getAllDataFromJsonApi2;
+
+        public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
+        {
+            ArgumentNullException.ThrowIfNull(execute, nameof(execute));
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Func<Task<string>> getAllDataFromJsonApi, Action check)
+        {
+            this.getAllDataFromJsonApi = getAllDataFromJsonApi;
+            this.check = check;
+        }
+
+        public RelayCommand(Func<Task<string>> getAllDataFromJsonApi, Func<object?, Task<bool>> check1)
+        {
+            this.getAllDataFromJsonApi = getAllDataFromJsonApi;
+            this.check1 = check1;
+        }
+
+        public RelayCommand(Action getAllDataFromJsonApi1, Func<object?, Task<bool>> check2)
+        {
+            this.getAllDataFromJsonApi1 = getAllDataFromJsonApi1;
+            this.check2 = check2;
+        }
+
+        public RelayCommand(Action getAllDataFromJsonApi2)
+        {
+            this.getAllDataFromJsonApi2 = getAllDataFromJsonApi2;
+        }
+
+        public bool CanExecute(object? parameter)
+            => _canExecute == null || _canExecute(parameter);
+
+        public void Execute(object? parameter)
+            => _execute(parameter);
+    }
+}
---
+++ WpfApp2/ViewModels/WindowViewModels/MainWindowViewModel.cs
@@ -0,0 +1,183 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using WpfApp2.Commands;
+using WpfApp2.Data;
+using WpfApp2.Services;
+using WpfApp2.Views.Windows;
+
+namespace WpfApp2.ViewModels.WindowViewModels
+{
+    
+
+    public class MainWindowViewModel:NotificationService
+    {
+        private string _Api { get; set; }
+        public string Api
+        {
+            get { return _Api; }
+            set { _Api = value;OnPropertyChanged(); }
+        }
+        public ObservableCollection<Comment> comments { get; set; }
+        public ICommand? GetApiCommand { get; set; }
+        public ICommand? RemoveCommand { get; set; }
+
+        private Comment _Comment;
+
+        public Comment Comment
+        {
+            get { return _Comment; }
+            set { _Comment = value; OnPropertyChanged(); }
+        }
+
+        public Window1 mainWindow { get; set; }
+
+        public MainWindowViewModel(Window1 _mainWindow)
+        {
+
+            RemoveCommand = new RelayCommand(
+                async remove =>
+                {
+                    await Remove();
+                },
+                a => true);
+            mainWindow = _mainWindow;
+            comments = new ObservableCollection<Comment>();
+
+            GetApiCommand = new RelayCommand(GetAllDataFromJsonApi);
+        }
+        private async Task<bool> Check(object? parameter)
+        {
+
+            if (Api != null)
+            {
+                string fileName = "comments.json";
+
+                string directoryPath = @"../../../Database";
+
+                string filePath = System.IO.Path.Combine(directoryPath, fileName);
+
+                if (File.Exists(filePath))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+               
+            }
+            return false;
+        }
+
+        private async void GetAllDataFromJsonApi(object? param)
+        {
+            string apiUrl = "https://jsonplaceholder.typicode.com/comments";
+            string FileName = "comments.json";
+
+            string directoryPath = @"../../../Database";
+
+            string filePath = System.IO.Path.Combine(directoryPath, FileName);
+
+            if (!File.Exists(filePath))
+            {
+                try
+                {
+                    using var httpClient = new HttpClient();
+
+                    HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonString = await response.Content.ReadAsStringAsync();
+
+                        File.WriteAllText(filePath,jsonString);
+                        var commentsFromFile = JsonConvert.DeserializeObject<ObservableCollection<Comment>>(filePath);
+
+                        string serializedJson = System.Text.Json.JsonSerializer.Serialize(filePath);
+
+                        File.WriteAllText(FileName, serializedJson);
+                        await Task.Delay(5000);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
+
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    string json = File.ReadAllText(filePath);
+                    var commentsFromFile = JsonConvert.DeserializeObject<ObservableCollection<Comment>>(json);
+                    foreach (var comment in commentsFromFile!)
+                    {
+                        comments!.Add(comment);
+                        await Task.Delay(10);
+                    }
+                }
+                else
+                    MessageBox.Show($"File not found: {filePath}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading cars from JSON file: {ex.Message}");
+            }
+
+
+        }
+
+
+
+
+        private async Task Remove()
+        {
+            if (comments == null)
+                return;
+
+            string FileName = "comments.json";
+
+            string directoryPath = @"../../../Database";
+
+            string filePath = System.IO.Path.Combine(directoryPath, FileName);
+
+            try
+            {
+                Comment? selectedItem = mainWindow.List.SelectedItem as Comment;
+
+                if (selectedItem != null )
+                {
+                    comments.Remove(selectedItem);
+                    string json = JsonConvert.SerializeObject(comments, Formatting.Indented);
+                    await File.WriteAllTextAsync(filePath!, json);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}");
+            }
+        }
+    }
+
+
+
+
+    }
